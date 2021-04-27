560c5cc273426a313823b843cacaa1437d9f61a8
Sabzs <sajiyoonline@gmail.com>
2021-04-27 10:19:32 -0700

    refactored bookDetails component back into BookList component

--- src/components/BookDetails.js
+++ src/components/BookDetails.js
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react'
-import { BookContext } from '../context/BookContext'
+// import React from 'react'
+// // import React, { useContext } from 'react'
+// // import { BookContext } from '../context/BookContext'
 
-const BookDetails = ({ book }) => {
-  const { dispatch } = useContext(BookContext);
+// const BookDetails = ({ book }) => {
+//   // const { dispatch } = useContext(BookContext);
 
+//   return (
+//     // <li className="bookDetails" onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}>
+//     <li className="bookDetails" >
+//       <h2 className="bookDetails__title" >{book.title}</h2>
+//       <h4 className="bookDetails__author" >{book.author}</h4>
+//     </li>
+//   )
+// }
 
-
-  return (
-    <li className="bookList__list-Details" onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}>
-      <h2 className="bookList__title" >{book.title}</h2>
-      <h4 className="bookList__author" >{book.author}</h4>
-    </li>
-  )
-}
-
-export default BookDetails
+// export default BookDetails
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,28 +1,59 @@
-import React, { useContext } from 'react'
-import { BookContext } from '../context/BookContext'
-import BookDetails from './BookDetails'
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { BookContext } from '../context/BookContext';
+// import BookDetails from './BookDetails'
 
 const BookList = () => {
-  const { books } = useContext(BookContext);
-
+  const { books, dispatch } = useContext(BookContext);
 
 
   return books.length ? (
+    // <div className="bookList" >
+    // <ul className="bookList__listContainer" >
     <div className="bookList" >
-      <ul className="bookList__listContainer" >
-        {books.map(book => {
-          return (<BookDetails book={book} key={book.id} />);
-        })}
-      </ul>
+
+      {books.map(book => {
+        return (
+          <div className="bookList__booklist-wrapper" key={book.id}>
+
+            {/* <BookDetails book={book} /> */}
+
+            {/* <div className="bookList__booklist-wrapper" > */}
+            <div className="bookList__list" >
+              <h2 className="bookList__title" >{book.title}</h2>
+              <h4 className="bookList__author" >{book.author}</h4>
+            </div>
+
+            <div className="bookList__edit-delete-wrapper" >
+              <button className="bookList__edit-btn" >
+                <Link to={`/editbook/${book.id}`} >Edit </Link>
+              </button>
+              <button className="bookList__delete-btn"
+                onClick={() => dispatch({ type: "REMOVE_BOOK", id: book.id })}
+              > Delete
+              </button>
+            </div>
+
+
+
+          </div>
+
+        );
+
+      })}
+
+
+
     </div>
+    // {/* </div> */ }
 
   ) : (
-      <div className="emptyList" >
-        <div className="emptyList__infoDiv"  >
-          <h3 className="emptyList__info" > <span>Hooray...!!</span> your list is empty, please add some more books. </h3>
-        </div>
+    <div className="emptyList" >
+      <div className="emptyList__infoDiv"  >
+        <h3 className="emptyList__info" > <span>Hooray...!!</span> your list is empty, please add some more books. </h3>
       </div>
-    )
+    </div>
+  )
 }
 
 export default BookList
