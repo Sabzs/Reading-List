3183c8632e59369f5f311b46b198a4d6ed0f877b
Sabzs <sajiyoonline@gmail.com>
2021-05-01 22:29:19 -0700

    cleanup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const colors = require("colors");
-const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
