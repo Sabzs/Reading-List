f9f6383a2e46192594de36763c8b1aa3d44b5602
Sabzs <sajiyoonline@gmail.com>
2021-05-01 22:30:23 -0700

    commented out editBook route

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home';
 import AddBook from './components/AddBook';
-import EditBook from './components/EditBook';
+// import EditBook from './components/EditBook';
 import Navbar from './components/Navbar';
 import BookContextProvider from './context/BookContext';
 import "./styles/App.css";
+
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
             <Switch>
               <Route path="/" exact render={() => <Home />} />
               <Route path="/addbook" exact render={() => <AddBook />} />
-              <Route path="/editbook/:id" exact render={() => <EditBook />} />
+              {/* <Route path="/editbook/:id" exact render={() => <EditBook />} /> */}
             </Switch>
           </Router>
         </div>
