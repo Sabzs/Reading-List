8303b07db090913f7d0c7c213b1c7f4723425dba
Sabzs <sajiyoonline@gmail.com>
2021-05-01 22:19:23 -0700

    commented out editBook context and reducer

--- client/src/context/BookContext.js
+++ client/src/context/BookContext.js
@@ -1,49 +1,105 @@
 import React, { createContext, useReducer } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
+import axios from 'axios';
+// import { useParams } from 'react-router';
 
 
 //initial state
-const initialState = { books: [] };
+const initialState = {
+  books: [],
+  error: null,
+  loading: true
+};
 
 //create Context
 export const BookContext = createContext(initialState);
 
 //global provider
-const BookContextProvider = ({ children }) => {
+const BookContextProvider = (props) => {
   const [state, dispatch] = useReducer(bookReducer, initialState);
 
 
   //==Actions==//
-  const addBook = (book) => {
-    dispatch({
-      type: "ADD_BOOK",
-      payload: book
-    })
+  const getBooks = async () => {
+    try {
+      const res = await axios.get("/api/books/getbooks");
+      console.log(res.data)
+      dispatch({
+        type: "GET_BOOKS",
+        payload: res.data.getbooks
+      })
+    } catch (err) {
+      dispatch({
+        type: "BOOK_ERROR",
+        payload: err.response.data.error
+      });
+    }
   }
 
-  const editBook = (book) => {
-    dispatch({
-      type: "EDIT_BOOK",
-      payload: book
-    })
+
+  const addBook = async (book) => {
+    const config = { headers: { 'Content-Type': 'application/json' } };
+    try {
+      const res = await axios.post("/api/books/newbook", book, config);
+      console.log(res.data.newbook)
+      dispatch({
+        type: "ADD_BOOK",
+        payload: res.data.newbook
+      })
+    } catch (err) {
+      dispatch({
+        type: "BOOK_ERROR",
+        payload: err.response.data.error
+      });
+    }
   }
 
-  const deleteBook = (id) => {
-    dispatch({
-      type: "DELETE_BOOK",
-      payload: id
-    });
+  // // const { id } = useParams;
+  // const editBook = async (book) => {
+  //   const config = { headers: { 'Content-Type': 'application/json' } };
+  //   try {
+  //     const res = await axios.put(`/api/books/updatebook/${book.id}`, book, config)
+  //     dispatch({
+  //       type: "EDIT_BOOK",
+  //       payload: res.data.updatedbook
+  //     })
+  //     getBooks();
+  //   } catch (err) {
+  //     dispatch({
+  //       type: "BOOK_ERROR",
+  //       payload: err.response.data.error
+  //     });
+  //   }
+  // }
+
+  const deleteBook = async (id) => {
+    try {
+      await axios.delete(`/api/books/deleteBook/${id}`)
+      dispatch({
+        type: "DELETE_BOOK",
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: "BOOK_ERROR",
+        payload: err.response.data.error
+      });
+    }
+
   }
 
   return (
     <div>
       <BookContext.Provider value={{
         books: state.books,
+        error: state.error,
+        loading: state.loading,
+        getBooks,
         addBook,
-        editBook,
+        // editBook,
         deleteBook
       }}>
-        {children}
+        {props.children}
       </BookContext.Provider>
 
     </div>
--- client/src/reducers/bookReducer.js
+++ client/src/reducers/bookReducer.js
@@ -1,29 +1,42 @@
 export const bookReducer = (state, action) => {
   switch (action.type) {
 
+    case "GET_BOOKS":
+      return {
+        ...state,
+        loading: false,
+        books: action.payload
+      }
+
     case "ADD_BOOK":
       return {
         ...state,
-        books: [action.payload, ...state.books]
+        books: [...state.books, action.payload]
       }
 
-    case "EDIT_BOOK":
-      const updatedBook = action.payload;
-      const updateBooks = state.books.map(book => {
-        if (book.id === updatedBook.id) {
-          return updatedBook
-        }
-        return book
-      })
-      return {
-        ...state,
-        books: updateBooks
-      }
+    // case "EDIT_BOOK":
+    //   const updatedBook = action.payload;
+    //   const updatedBooks = state.books.map(book => {
+    //     if (book.id === updatedBook.id) {
+    //       return updatedBook
+    //     }
+    //     return book
+    //   })
+    //   return {
+    //     ...state,
+    //     books: updatedBooks
+    //   }
 
     case "DELETE_BOOK":
       return {
         ...state,
         books: state.books.filter(book => book.id !== action.payload)
+      }
+
+    case "BOOK_ERROR":
+      return {
+        ...state,
+        error: action.payload
       }
 
     default:
